d836b351917e4ffb7cd2471cf08381031ec3b34a
sonoda <anko1020s@smail.com>
2023-04-22 23:19:06 +0900

    excel

--- attendance/views.py
+++ attendance/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.utils import timezone
 from openpyxl import Workbook, load_workbook
 from reportlab.pdfgen import canvas
-import win32com.client
+
 from pathlib import Path
 import pythoncom
 from . import system
@@ -123,22 +123,26 @@ class Registration(TemplateView):
         
 def PDF(request,user):
     
-    pythoncom.CoInitialize()
-    excel = win32com.client.Dispatch("Excel.Application")
     dir = r'excel_sheets/wageTime_sheets 2023/'
     path_excel = str(Path(dir+'time sheet 4.xlsx').resolve())
     path_pdf = str(Path(dir+'pdf_temp.pdf').resolve())
+    '''pythoncom.CoInitialize()
+    excel = win32com.client.Dispatch("Excel.Application")
     _wb = excel.Workbooks.Open(path_excel)
     ws = _wb.Worksheets(user)
     ws.ExportAsFixedFormat(0,path_pdf)
     _wb.Close()
     excel.Quit()
 
-    pythoncom.CoUninitialize() 
+    pythoncom.CoUninitialize() '''
+    '''
+    wb = load_workbook(dir+'time sheet 4.xlsx')
+    ws = wb['s']
+    wb.save(dir+'pdf_temp.pdf',SaveFormat.PDF)'''
             
-    response = HttpResponse(open(path_pdf, 'rb').read(), content_type='application/pdf')
-    pdf_name = "Time Sheet:"+"s"+".pdf"
-    response['Content-Disposition'] = ' filename=pdf_name'
+    response = HttpResponse(open(path_excel, 'rb').read(), content_type='application/vnd.ms-excel')
+    pdf_name = "Time Sheet:"+"s"+".xlsx"
+    response['Content-Disposition'] = 'attachment filename=pdf_name'
     return response
 
 def control(request):
